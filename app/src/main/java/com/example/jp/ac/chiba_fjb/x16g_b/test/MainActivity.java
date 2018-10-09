e068fcfc5bacb1502f7b5dfcc8ab50424790ab38
WIN_NT\x16g019 <x16g019@chiba-fjb.ac.jp>
2018-10-09 09:47:50 +0900

    10/9

--- app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/MainActivity.java
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.jp.ac.chiba_fjb.x16g_b.test;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Handler mHandler;
+    private Timer mTimer;
+    // 時刻表示のフォーマット
+    private static SimpleDateFormat mSimpleDataFormat = new SimpleDateFormat("yyyy年　MM月dd日　HH:mm:ss");
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandler = new Handler();
+        mTimer = new Timer();
+
+        // 一秒ごとに定期的に実行します。
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    public void run() {
+                        Calendar calendar = Calendar.getInstance();
+                        String nowDate = mSimpleDataFormat.format(calendar.getTime());
+                        // 時刻表示をするTextView
+                        ((TextView) findViewById(R.id.clock)).setText(nowDate);
+                    }
+                });}
+        },0,1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 定期実行をcancelする
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
     }
 }
+
