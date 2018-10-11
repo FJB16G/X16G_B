aef2e4d8492a4c27918b3487e4fb0d417f3ac18c
WIN_NT\x16g022 <x16g022@chiba-fjb.ac.jp>
2018-10-11 15:03:10 +0900

    音声・テキスト画面です。背景は白色ですがおそらく統合できます。多分

--- app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/MainActivity.java
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/MainActivity.java
@@ -2,85 +2,35 @@ package com.example.jp.ac.chiba_fjb.x16g_b.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.speech.RecognizerIntent;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int REQUEST_CODE = 1;
-    private EditText inpText;
-    private String preInp = "";
-    private Button inpBtn, canBtn, clsBtn;
-    /** Called when the activity is first created. */
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.RecognizerIntentTest);
-
-        inpText = (EditText)findViewById(R.id.result_id);
-        inpBtn = (Button)findViewById(R.id.speech_id);
-        inpBtn.setOnClickListener(this);
-        canBtn = (Button)findViewById(R.id.cancel_id);
-        canBtn.setOnClickListener(this);
-        clsBtn = (Button)findViewById(R.id.clear_id);
-        clsBtn.setOnClickListener(this);
+        setContentView(R.layout.voicetext);
+        // 表示内容の切替え
+        final ImageView button = (ImageView) findViewById(R.id.soundButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            int start = 1;
+            @Override
+            public void onClick(View view) {
+                if(start == 1){
+                    button.setImageResource(R.drawable.pause);
+                    start = 0;
+                }else {
+                    button.setImageResource(R.drawable.start);
+                    start = 1;
+                }
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        // TODO Auto-generated method stub
-        if(view == inpBtn){
-            try{
-                // 音声認識の準備
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                // Intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "音声を入力してください。");
-                // インテント発行
-                startActivityForResult(intent, REQUEST_CODE);
-            }catch (ActivityNotFoundException e){
-                Toast.makeText(MainActivity.this, "Not found Activity", Toast.LENGTH_LONG).show();
-            }
-        }else if(view == canBtn) {
-            if(preInp != null){
-                inpText.setText(preInp);// 前の入力文字列を表示
-                inpText.setSelection(preInp.length());// カーソルを移動
-            }else if(view == clsBtn) {
-                inpText.setText("");
-            }
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String input = "";
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
-            preInp = inpText.getText().toString();
-            // 認識結果を取得
-            ArrayList<String> candidates = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            Log.v("Speech", "Candidate Num = " + candidates.size());
-            if(candidates.size() > 0) {
-                input = preInp + candidates.get(0);// 認識結果(1位候補)
-            }
-            if(input != null) {
-                inpText.setText(input);// 入力文字列を表示
-                inpText.setSelection(input.length());// カーソルを移動
-            }
-
-        }
-    }
 
 }
 
