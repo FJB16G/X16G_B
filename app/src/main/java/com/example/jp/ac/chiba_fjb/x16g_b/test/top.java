2b46cd61819e2aa9efdec92e73cd292627e75bc7
WIN_NT\x16g029 <rinrin7900@gmail.com>
2018-10-11 10:21:01 +0900

    修正

--- app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/top.java
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/top.java
@@ -2,14 +2,13 @@ package com.example.jp.ac.chiba_fjb.x16g_b.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class top extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.top);
 
     }
 }
