3bd2ab4873ca45ba046bc9a09d2f029f00237cc2
WIN_NT\x16g029 <rinrin7900@gmail.com>
2018-10-15 15:17:54 +0900

    ばーじょんあっぷ

--- app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/top.java
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/top.java
@@ -8,7 +8,7 @@ public class top extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.top);
+
 
     }
 }
