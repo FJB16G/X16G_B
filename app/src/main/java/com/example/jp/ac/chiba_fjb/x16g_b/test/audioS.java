f39b7ea0c904890d2bf0d1aed6961c443ad13bf8
tm <rinrin7900@gmail.com>
2018-11-14 10:17:08 +0900

    再生画面完成

---
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/audioS.java
@@ -0,0 +1,113 @@
+package com.example.jp.ac.chiba_fjb.x16g_b.test;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class saisei extends AppCompatActivity {
+    private MediaPlayer mediaPlayer ;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.saisei);
+
+        ImageButton s_button = findViewById(R.id.start);
+
+        s_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 音楽再生
+                audioPlay();
+            }
+        });
+        // 音楽停止ボタン
+        ImageButton buttonStop = findViewById(R.id.stop);
+
+        // リスナーをボタンに登録
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    // 音楽停止
+                    audioStop();
+                }
+            }
+        });
+
+    }
+    private boolean audioSetup(){
+        boolean fileCheck = false;
+
+        // インタンスを生成
+        mediaPlayer = new MediaPlayer();
+
+        //音楽ファイル名, あるいはパス
+        String filePath = "sample.mp3";
+
+        // assetsから mp3 ファイルを読み込み
+        try(AssetFileDescriptor afdescripter = getAssets().openFd(filePath);)
+        {
+            // MediaPlayerに読み込んだ音楽ファイルを指定
+            mediaPlayer.setDataSource(afdescripter.getFileDescriptor(),
+                    afdescripter.getStartOffset(),
+                    afdescripter.getLength());
+            // 音量調整を端末のボタンに任せる
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mediaPlayer.prepare();
+            fileCheck = true;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return fileCheck;
+    }
+    private void audioPlay(){
+        if(mediaPlayer == null){
+            if(audioSetup()){
+                Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }else{
+            // 繰り返し再生する場合
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            // リソースの解放
+            mediaPlayer.release();
+        }
+        //再生する
+        mediaPlayer.start();
+
+        //終了を検知するリスナー
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Log.d("debug","end of audio");
+                audioStop();
+            }
+        });
+    }
+    private void audioStop() {
+        // 再生終了
+        mediaPlayer.stop();
+        // リセット
+        mediaPlayer.reset();
+        // リソースの解放
+        mediaPlayer.release();
+
+        mediaPlayer = null;
+    }
+}
---
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/voice_Search.java
@@ -0,0 +1,4 @@
+package com.example.jp.ac.chiba_fjb.x16g_b.test;
+
+public class voice_Search {
+}
