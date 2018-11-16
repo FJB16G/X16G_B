22a92f7efcd80dac1af975b952ca5d03cf4328c5
WIN_NT\x16g022 <x16g022@chiba-fjb.ac.jp>
2018-11-16 11:51:25 +0900

    録音の機能には「Voice/voice」文字起こしの機能には「Text/text」と前につけてます。 どちらも単一では動きました単一では。結合時は知りません。文字起こし画面は大半が及川先生なのでご容赦ください。

---
+++ app/src/main/java/com/example/jp/ac/chiba_fjb/x16g_b/test/TextSpaceDialogFlagment.java
@@ -0,0 +1,66 @@
+package com.example.jp.ac.chiba_fjb.x16g_b.test;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.widget.EditText;
+
+public class TextSpaceDialogFlagment extends DialogFragment {
+    private EditText editText;
+
+    // ダイアログが生成された時に呼ばれるメソッド ※必須
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // ダイアログ生成  AlertDialogのBuilderクラスを指定してインスタンス化します
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity()).setCancelable(false);
+        // タイトル設定
+        dialogBuilder.setTitle("タイトルが空白です");
+        // 表示する文章設定
+        dialogBuilder.setMessage("タイトルを入力してください");
+
+        // 入力フィールド作成/
+       /* final EditText editText = new EditText(getActivity());
+        editText.setText("");
+        dialogBuilder.setView(editText);*/
+        // OKボタン作成
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               /* // editTextから値を取得
+                String returnValue = editText.getText().toString();
+                // ダイアログクラスをインスタンス化
+                TextFragment textFragment = new TextFragment();
+                // ダイアログに値を渡す
+                Bundle bundle = new Bundle();
+                // キーと値の順で渡す
+                bundle.putString("Title", editText.getText().toString());
+                // 値をセット
+                textFragment.setTextView(bundle);*/
+
+
+
+
+
+            }
+
+        });
+
+        /*// NGボタン作成
+        dialogBuilder.setNegativeButton("NG", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 何もしないで閉じる
+            }
+        });*/
+
+        // dialogBulderを返す
+        return dialogBuilder.create();
+
+    }
+
+
+}
